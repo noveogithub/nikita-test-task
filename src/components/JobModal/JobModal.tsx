f800e34ffe7366662a080cf9ab482a7007af719a
NKBelousov <belousovnikita92@gmail.com>
2020-01-23 09:05:12 +0300

    add modal open & close animation

--- src/components/JobModal/JobModal.tsx
+++ src/components/JobModal/JobModal.tsx
@@ -1,6 +1,6 @@
-import React, { memo, useEffect, useCallback } from "react";
+import React, { memo, useEffect, useCallback, useState } from "react";
 import { Button, Box, Text } from "welcome-ui";
-import styled from "@xstyled/styled-components";
+import styled, { keyframes } from "@xstyled/styled-components";
 
 import { IJob } from "@app/types/IJob";
 import { getApplyUrl } from "@app/utils/getApplyUrl";
@@ -8,6 +8,18 @@ import { getApplyUrl } from "@app/utils/getApplyUrl";
 type JobModalProps = IJob & {
   onClose: () => void;
 };
+
+const ANIMATION_DURATION = 300;
+
+const appear = keyframes`
+  0% {
+    transform: scale(0);
+  }
+
+  100% {
+    transform: scale(1);
+  }
+`;
 
 const Overlay = styled.div`
   align-items: center;
@@ -34,13 +46,27 @@ const ButtonWrapper = styled.div`
 `;
 
 const Wrapper = styled.div`
+  animation: ${appear} ${ANIMATION_DURATION / 1000}s
+    cubic-bezier(0.28, 0.84, 0.42, 1);
+  animation-fill-mode: forwards;
   max-height: 100%;
   overflow: auto;
+`;
+
+const ClosingWrapper = styled(Wrapper)`
+  animation-direction: reverse;
 `;
 
 // Component to render currently opened job offer
 export const JobModal: React.FC<JobModalProps> = memo(
   ({ onClose, name, description, profile, websites_urls }) => {
+    const [isClosing, setIsClosing] = useState<boolean>(false);
+
+    const closeCallback = useCallback(() => {
+      setIsClosing(true);
+      setTimeout(onClose, ANIMATION_DURATION);
+    }, [onClose]);
+
     // Prevent default click on overlay
     const prevent = useCallback((e: Event) => {
       e.stopPropagation();
@@ -65,9 +91,11 @@ export const JobModal: React.FC<JobModalProps> = memo(
       };
     }, []);
 
+    const Tag = isClosing ? ClosingWrapper : Wrapper;
+
     return (
-      <Overlay onClick={onClose}>
-        <Wrapper>
+      <Overlay onClick={closeCallback}>
+        <Tag>
           <Box backgroundColor="light.900" onClick={prevent} padding="15px">
             <Text variant="h1">{name}</Text>
             <Text variant="h2">Job Description</Text>
@@ -80,7 +108,7 @@ export const JobModal: React.FC<JobModalProps> = memo(
               </Button>
             </ButtonWrapper>
           </Box>
-        </Wrapper>
+        </Tag>
       </Overlay>
     );
   }
